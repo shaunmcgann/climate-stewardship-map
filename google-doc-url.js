396a1629e7793bc07a3aa2af666371893cbb8023
HartfordClimate <hartfordclimate@gmail.com>
2017-05-23 15:21:42 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = ' https://docs.google.com/spreadsheets/d/1C74lfKAh09c6Au3hAo5S_cslTuZZiC3ZKCieiLp4Mmw/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1C74lfKAh09c6Au3hAo5S_cslTuZZiC3ZKCieiLp4Mmw/edit#gid=0';
